3a6ad80503006d1e69180b62c08b6d927eaf4c36
Ishini-0313 <misnfernando1@gmail.com>
2026-02-28 07:40:48 +0530

    removed

--- app/(protected)/upgradePlan.jsx
+++ app/(protected)/upgradePlan.jsx
@@ -1,48 +1,48 @@
-import React, { useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import { useRouter } from "expo-router";
+// import React, { useState } from "react";
+// import { Pressable, StyleSheet, Text, View } from "react-native";
+// import { useRouter } from "expo-router";
 
-const plans = [
-  { id: "go", name: "Go", price: 2000 },
-  { id: "plus", name: "Plus", price: 4000 },
-  { id: "pro", name: "Pro", price: 8000 },
-];
+// const plans = [
+//   { id: "go", name: "Go", price: 2000 },
+//   { id: "plus", name: "Plus", price: 4000 },
+//   { id: "pro", name: "Pro", price: 8000 },
+// ];
 
-export default function UpgradePlan() {
-  const [selectedPlan, setSelectedPlan] = useState(null);
-  const router = useRouter();
+// export default function UpgradePlan() {
+//   const [selectedPlan, setSelectedPlan] = useState(null);
+//   const router = useRouter();
 
-  const handlePlanSelect = (plan) => {
-    setSelectedPlan(plan.id);
-    router.push({
-      pathname: "/payment",
-      params: {
-        planName: plan.name,
-        planPrice: plan.price,
-      },
-    });
-  };
+//   const handlePlanSelect = (plan) => {
+//     setSelectedPlan(plan.id);
+//     router.push({
+//       pathname: "/payment",
+//       params: {
+//         planName: plan.name,
+//         planPrice: plan.price,
+//       },
+//     });
+//   };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.pageTitle}>Upgrade Plan</Text>
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.pageTitle}>Upgrade Plan</Text>
 
-      {plans.map((plan) => (
-        <Pressable
-          key={plan.id}
-          style={styles.btn}
-          onPress={() => handlePlanSelect(plan)}
-        >
-          <Text style={styles.label}>{plan.name}</Text>
-        </Pressable>
-      ))}
-    </View>
-  );
-}
+//       {plans.map((plan) => (
+//         <Pressable
+//           key={plan.id}
+//           style={styles.btn}
+//           onPress={() => handlePlanSelect(plan)}
+//         >
+//           <Text style={styles.label}>{plan.name}</Text>
+//         </Pressable>
+//       ))}
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({ 
-    container: { padding: 20, }, 
-    pageTitle: { fontSize: 30, marginBottom: 30 }, 
-    btn: { borderWidth: 1, borderRadius: 10, width: "30%", padding:10, marginBottom: 30, }, 
-    label: { fontSize: 20 } 
-});
+// const styles = StyleSheet.create({ 
+//     container: { padding: 20, }, 
+//     pageTitle: { fontSize: 30, marginBottom: 30 }, 
+//     btn: { borderWidth: 1, borderRadius: 10, width: "30%", padding:10, marginBottom: 30, }, 
+//     label: { fontSize: 20 } 
+// });
